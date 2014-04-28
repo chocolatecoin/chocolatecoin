ec2f76bd51ee0c695cfaf05bfd469bec6049fba9
Benz Muircroft <benzmuircroft@gmail.com>
2014-04-29 01:39:01 +0200

    BENZ

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -34,9 +34,9 @@ QString BitcoinUnits::name(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("SMC");
-    case mBTC: return QString("mSMC");
-    case uBTC: return QString::fromUtf8("μSMC");
+    case BTC: return QString("FAB");
+    case mBTC: return QString("mFAB");
+    case uBTC: return QString::fromUtf8("μFAB");
     default: return QString("???");
     }
 }
@@ -45,9 +45,9 @@ QString BitcoinUnits::description(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("SmallChange");
-    case mBTC: return QString("Milli-SmallChange (1 / 1,000)");
-    case uBTC: return QString("Micro-SmallChange (1 / 1,000,000)");
+    case BTC: return QString("FabCoin");
+    case mBTC: return QString("Milli-FabCoin (1 / 1,000)");
+    case uBTC: return QString("Micro-FabCoin (1 / 1,000,000)");
     default: return QString("???");
     }
 }
