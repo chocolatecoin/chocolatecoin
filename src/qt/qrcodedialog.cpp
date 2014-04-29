479cde51d7dcabe618fa91fa06cfa3d2b480a338
benzmuircroft <benz.stackoverflow@gmail.com>
2014-04-29 23:41:07 +0200

    Chocolate

--- src/qt/qrcodedialog.cpp
+++ src/qt/qrcodedialog.cpp
@@ -83,7 +83,7 @@ void QRCodeDialog::genCode()
 
 QString QRCodeDialog::getURI()
 {
-    QString ret = QString("fabcoin:%1").arg(address);
+    QString ret = QString("chocolatecoin:%1").arg(address);
     int paramCount = 0;
 
     ui->outUri->clear();
