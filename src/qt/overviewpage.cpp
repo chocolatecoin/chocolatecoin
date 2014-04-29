cc8848f62b5c76bdcc9339c78a83dbdd06b0ae33
benzmuircroft <benz.stackoverflow@gmail.com>
2014-04-29 23:39:37 +0200

    Chocolate

--- src/qt/overviewpage.cpp
+++ src/qt/overviewpage.cpp
@@ -175,7 +175,7 @@ void OverviewPage::setModel(WalletModel *model)
         connect(model->getOptionsModel(), SIGNAL(displayUnitChanged(int)), this, SLOT(updateDisplayUnit()));
     }
 
-    // update the display unit, to not use the default ("FAB")
+    // update the display unit, to not use the default ("CHO")
     updateDisplayUnit();
 }
 
